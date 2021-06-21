3534bd57fc866a71add3a6a60f4c392fa3a4a044
Casey Pollock <contactus@nearfuture.marketing>
2021-06-20 21:29:50 -0700

    Initial Commit

---
+++ ML Trainer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ML Trainer
+//
+//  Created by Casey Pollock on 6/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ML Trainer/ML_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ML_TrainerApp.swift
+//  ML Trainer
+//
+//  Created by Casey Pollock on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct ML_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
